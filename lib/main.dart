5e39028093e053000c3e4735ed67d9714f35ac51
adnanganie <ganieadnan10@gmail.com>
2019-10-19 13:02:37 +0530

    Material card added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 backgroundImage: AssetImage('images/adnan.png'),
@@ -19,7 +21,7 @@ class MyApp extends StatelessWidget {
                 'Adnan Ayoub',
                 style: TextStyle(
                   fontFamily: 'Pacifico-Regular',
-                  fontSize: 30.0,
+                  fontSize: 28.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
@@ -27,28 +29,78 @@ class MyApp extends StatelessWidget {
               Text(
                 'SOFTWARE ENGINEER',
                 style: TextStyle(
-                    fontSize: 20.0,
+                    fontFamily: 'Source Sans Regular',
+                    fontSize: 18.0,
+                    color: Colors.white70,
                     fontWeight: FontWeight.bold,
-                    letterSpacing: 2),
+                    letterSpacing: 1),
               ),
-              Container(
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      '+91 8825070059',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.teal.shade900,
-                        fontWeight: FontWeight.bold
-                      ),
-                    )
-                  ],
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
                 ),
-              )
+              ),
+//              Container(
+//                color: Colors.white,
+//                margin: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+//                padding: EdgeInsets.all(10.0),
+//                child: Row(
+//                  children: <Widget>[
+//                    Icon(
+//                      Icons.phone,
+//                      color: Colors.teal,
+//                    ),
+//                    SizedBox(
+//                      width: 10.0,
+//                    ),
+//                    Text(
+//                      '+91 8825070059',
+//                      style: TextStyle(
+//                        fontFamily: 'Source Sans Regular',
+//                        fontSize: 16.0,
+//                        color: Colors.teal.shade900,
+//                        fontWeight: FontWeight.bold,
+//                      ),
+//                    )
+//                  ],
+//                ),
+//              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.fromLTRB(25.0, 10.0, 25.0, 10.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text('+91 8825070059',
+                        style: TextStyle(
+                          fontFamily: 'Source Sans Regular',
+                          fontSize: 16.0,
+                          color: Colors.teal.shade900,
+                          fontWeight: FontWeight.bold,
+                        ))),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'adnan@alkowsartech.com',
+                      style: TextStyle(
+                          fontFamily: 'Source Sans Regular',
+                          fontSize: 16.0,
+                          color: Colors.teal.shade900,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ))
             ],
           ),
         ),
